d922b17cd4d3df913bafc64b2c4c906dbea8a3c5
Yaroslav Karandashev <yarix7@gmail.com>
2013-02-25 22:38:37 +0400

    Add interfaces to Generic servlet

--- src/plainjavaservlet/SimpleServlet.java
+++ src/plainjavaservlet/SimpleServlet.java
@@ -1,13 +1,16 @@
 package plainjavaservlet;
 
 import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serializable;
 
-public class SimpleServlet extends GenericServlet {
+public class SimpleServlet extends GenericServlet implements Servlet, ServletConfig, Serializable {
 
 	private static final long serialVersionUID = 3948245126352292249L;
 
